d7b23859118d270b13a560fe6c0d5110fa16226e
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-24 18:28:23 +0300

    file: first commit

---
+++ partials/user.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./../css/styles.css">
+    <title>File | USER!</title>
+</head>
+<body>
+
+    <div class="container">
+        <h2>User Account</h2>
+
+        <div class="form">
+            <form method="post" action="./partials/login.php">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required>
+
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+                
+                <label for="password">Password confirm</label>
+                <input type="password" id="password" name="password" required>
+                
+                <input type="submit" value="Login">
+              </form>
+        </div>
+        
+       
+    
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="script.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
